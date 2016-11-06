6bc9d04c87d1b9e475675fadc89b4a5d36d23ee2
Ismel <calero098@icloud.com>
2016-11-05 21:04:01 -0400

    basics exercises finished

---
+++ basics.rb
@@ -0,0 +1,49 @@
+# 1 
+
+puts "Calero " + "Maika"
+
+# 2
+
+puts 1234 / 1000
+puts 1234 % 1000 / 100
+puts 1234 % 1000 % 100 / 10
+puts 1234 % 10
+
+# 3
+
+peliculas = {
+	mar: 1980,
+	peligroso: 2000,
+	distinta: 1989,
+	corazon: 2003
+}
+puts peliculas[:mar]
+puts peliculas[:peligroso]
+puts peliculas[:distinta]
+puts peliculas[:corazon]
+
+# 4
+
+dates = [1980, 2000, 1989, 2003]
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+
+# 5
+
+puts   5 * 4 * 3 * 2 * 1
+puts   6 * 5 * 4 * 3 * 2 * 1
+puts   7 * 6 * 5 * 4 * 3 * 2 * 1
+puts   8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+# 6
+
+puts 9.9 * 9.9
+puts 21.21 * 21.21
+puts 3.4 * 3.4
+
+# 7
+
+# wrong )  replace it with }
+
