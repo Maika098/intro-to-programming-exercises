690e44e7d61029990a6edb67e5ddd2dce67cf8d8
Ismel <calero098@icloud.com>
2016-11-06 17:27:13 -0500

    variables

---
+++ age.rb
@@ -0,0 +1,10 @@
+puts "How old are you?"
+yourAge = gets.chomp.to_i
+puts "In 10 years you will be"
+puts yourAge + 10
+puts "In 20 years you will be"
+puts yourAge + 20
+puts "In 30 years you will be"
+puts yourAge + 30
+puts "In 40 years you will be"
+puts yourAge + 40
---
+++ name.rb
@@ -0,0 +1,34 @@
+# 1
+
+puts "What's your name?"
+str = gets.chomp
+puts "Welcome to the club " + str 
+
+# # 2
+
+10.times do
+	puts str
+end
+
+# 3
+
+puts "Your name?"
+name = gets.chomp
+
+puts "Last name?"
+
+last = gets.chomp
+
+puts "Your full name is " + name + " " + last
+
+
+
+
+
+
+
+
+
+
+
+
