e059037f332fb6793e2720781c40e432eafabbc7
Ismel <calero098@icloud.com>
2016-11-06 17:50:28 -0500

    Mehods

---
+++ methods.rb
@@ -0,0 +1,21 @@
+# 1
+
+def gretting (name) 
+	puts "Hello " + name
+end
+gretting('Carlos')
+
+# 3
+
+def multiply(num1 , num2)
+	num1 * num2
+end
+puts multiply(8, 2)
+
+# 5
+
+def scream(words)
+	words = words + "!!!!"
+	puts words
+end
+scream("Yippeee")
