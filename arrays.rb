6efc6901a43b3f046a0790db96735132ec14a071
Ismel <calero098@icloud.com>
2016-11-09 17:29:15 -0500

    arrays

---
+++ arrays.rb
@@ -0,0 +1,40 @@
+# 1
+
+arr = [1,2,3,5,7,9,11]
+number = 3
+
+puts arr.include?(number)
+
+# 3
+arr = [["test", "hello", "world"],["example", "mem"]]
+puts arr.first.first
+
+# 6
+
+names = ['bob', 'joe', 'susan', 'margaret']
+names[3] = 'jody'
+
+
+# 7
+
+arr = [1,2,3,4,5]
+new_arr = []
+
+arr.each do |n|
+	new_arr << n + 2
+end
+p arr
+p new_arr
+
+
+
+
+
+
+
+
+
+
+
+
+
